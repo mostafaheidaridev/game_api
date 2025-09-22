e4d76a0dcfb6d1a5fd2499b11667c83c55d8ea13
Mh <168337014+mostafaheidaridev@users.noreply.github.com>
2025-09-22 22:03:00 +0200

    implement Genre, list and more

--- rawg-frontend/src/main.tsx
+++ rawg-frontend/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-import theme from "./thems.ts";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ColorModeScript initialColorMode={theme["config"].initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
