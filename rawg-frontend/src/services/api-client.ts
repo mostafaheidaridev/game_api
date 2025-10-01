81258dee6e0960e6219d376e40fd934011c312f1
Mh <168337014+mostafaheidaridev@users.noreply.github.com>
2025-10-01 13:46:20 +0200

    Switch to RAWG API for production

--- rawg-frontend/src/services/api-client.ts
+++ rawg-frontend/src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "https://api.rawg.io/api",
-  baseURL: import.meta.env["VITE_API_URL"],
+  baseURL: "https://api.rawg.io/api",
+  // baseURL: import.meta.env["VITE_API_URL"],
   params: {
     key: import.meta.env["VITE_API_KEY"],
   },
